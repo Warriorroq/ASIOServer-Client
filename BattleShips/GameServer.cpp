82c5b79961c77f45fad0fb87207dd7cf341516a1
Arthur <Warriorroq@gmail.com>
2022-05-05 22:28:14 +0200

    added net message

--- BattleShips/GameServer.cpp
+++ BattleShips/GameServer.cpp
@@ -2,35 +2,15 @@
 #include <iostream>
 #include <asio/ts/buffer.hpp>
 #include <asio/ts/internet.hpp>
+#include <NetMessage.h>
 GameServer* GameServer::instance = 0;
 
 GameServer::GameServer() {
+	_contextWork = new io_context::work(_context);
 	_endPoint = tcp::endpoint(ip::make_address("93.184.216.34", _currentErrorCode), 80);
 	_socket = new tcp::socket(_context);
 	_socket->connect(_endPoint, _currentErrorCode);
-	if (!_currentErrorCode)
-	{
-		std::cout << "connected" << std::endl;
-	}
-	else {
-		std::cout << "error:" << _currentErrorCode.message() << std::endl;
-	}
-	if (_socket->is_open())
-	{
-		std::string req = 
-			"GET /index.html HTTP/1.1\r\n"
-			"Host: example.com\r\n"
-			"Connection: close\r\n\r\n";
-		_socket->write_some(buffer(req.data(), req.length()));
-		_socket->wait(_socket->wait_read);
-		while (_socket->available() > 0)
-		{
-			std::vector<char> vBuffer(_socket->available());
-			_socket->read_some(buffer(vBuffer.data(), vBuffer.size()));
-			for (auto c : vBuffer)
-				std::cout << c;
-		}
-	}
+	_contextThread = std::thread([&]() {_context.run(); });
 }
 
 void GameServer::CreateServer() {
--- BattleShips/GameServer.h
+++ BattleShips/GameServer.h
@@ -2,8 +2,8 @@
 #ifdef _WIN32
 #define _WIN32_WINNT 0x0A00
 #endif 
-#define ASIO_STANDALONE
 #include <asio.hpp>
+#include <thread>
 using namespace asio;
 using namespace ip;
 class GameServer {
@@ -15,6 +15,8 @@ public:
 private:
 	error_code _currentErrorCode;
 	io_context _context;
+	io_context::work *_contextWork;
+	std::thread _contextThread;
 	tcp::endpoint _endPoint;
 	tcp::socket *_socket;
 	GameServer();
